bd8d3568ac9047100dfd121ec34b6d666901510f
Aliaksandr Shasholko <axl_grim@Aliaksandrs-MacBook-Pro-2.local>
2018-07-16 22:31:31 +0200

    New Functionality
    Buttons are added, Pause panle is implemented, gyro detection changed to Y-axis only, animation for correct/wront answers is added.

--- HeadsUp/Assets/Scripts/GameManger.cs
+++ HeadsUp/Assets/Scripts/GameManger.cs
@@ -1,29 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
 using TMPro;
 
 public class GameManger : MonoBehaviour {
 
     private Gyroscope _gyro;
-    private int _scroeGuessed;
+    public bool IsPaused;
+    public GameObject PausePanel;
+
+    private Animator animator;
+    public int _scroreGuessed = 0;
+    public bool _guessed = false;
+    public bool _skipped = false;
     public bool gyro_enabled = false;
     public TextMeshProUGUI ScoreText;
-    private Quaternion _rotGuess = new Quaternion(0.0f, -0.9f, -0.5f, 0.0f);
-    private Quaternion _rotMissed = new Quaternion(0.0f, -0.5f, -0.9f,-0.1f);
+    public Image CorrectImage;
+    public Image WrongImage;
 
 	// Use this for initialization
 	void Start () 
     {
+        
+        //CorrectImage.enabled = false;
+        PausePanel.SetActive(false);
         EnableGyro();
+        animator = GetComponent<Animator>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        Debug.Log(_gyro.attitude);
-        CheckGuess();
-        ScoreText.text = _scroeGuessed.ToString();
+        if (!IsPaused)
+        {
+            Resume();
+
+        }
+        if (IsPaused)
+        {
+
+            Pause();
+
+        }
+
+        if(_gyro!= null)
+        {
+            CheckGuess();
+            CheckSkipped();
+            CheckDefault();
+        }
+        Debug.Log(_gyro.attitude.y);
+
+
 		
 	}
 
@@ -39,10 +71,77 @@ public class GameManger : MonoBehaviour {
 
     private void CheckGuess()
     {
-        if(_gyro.attitude == _rotGuess)
+        if(Math.Abs(_gyro.attitude.y) > 0.8f && !_guessed)
         {
-            _scroeGuessed += 1;
+            CorrectImage.enabled = true;
+            _scroreGuessed += 1;
+            _guessed = true;
+
+            ScoreText.text = _scroreGuessed.ToString();
+            if(CorrectImage.enabled)
+            {
+                animator.SetTrigger("Correct");
+            }
+
         }
 
+    }
+
+    private void CheckSkipped()
+    {
+        if (Math.Abs(_gyro.attitude.y) < 0.5f && !_skipped)
+        {
+            _skipped = true;
+            animator.SetTrigger("Wrong");
+
+        }
+
+
+    }
+    private void CheckDefault()
+    {
+        Debug.Log(_gyro.attitude);
+        if (Math.Abs(_gyro.attitude.y) <= 0.8f && Math.Abs(_gyro.attitude.y) >= 0.5f)
+        {
+            Debug.Log("Cleared");
+            _guessed = false;
+            _skipped = false;
+
+        }
+    }
+
+    void Resume()
+    {
+        PausePanel.SetActive(false);
+        Time.timeScale = 1f;
+
+    }
+
+    void Pause()
+    {
+        PausePanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void OnPauseBtnClicked()
+    {
+        IsPaused = true;
+
+    }
+
+    public void OnResumeBtnClicked()
+    {
+        IsPaused = false;
+
+    }
+
+    public void OnRestartBtnClicked()
+    {
+        SceneManager.LoadScene("HeadsUpScene");
+    }
+
+    public void OnMenuBtnClicked()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
